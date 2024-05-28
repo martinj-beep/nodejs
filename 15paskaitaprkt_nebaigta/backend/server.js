4220b1d2ffb429a9d2460f361d7d763cdd67329e
Martynas Jakutis <jakumart@gmail.com>
2024-05-28 23:50:21 +0300

    praktika

---
+++ 15paskaitaprkt_nebaigta/backend/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { PORT } = process.env;
+
+const app = express();
+
+app.use(express.json());
+
+app.use()
+
+app.listen(PORT, () => console.log(`App listening on PORT: ${PORT}`))
