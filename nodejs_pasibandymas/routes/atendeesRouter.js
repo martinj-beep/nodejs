ba453e9a486cd406b0a48789ef10fe00419d4863
Martynas Jakutis <jakumart@gmail.com>
2024-05-31 00:00:15 +0300

    changes

---
+++ nodejs_pasibandymas/routes/atendeesRouter.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/atendees", );
+router.post("/atendees", );
+router.put("/atendees/:id", );
+router.delete ("/atendees/:id", )
+
+export default router
---
+++ nodejs_pasibandymas/routes/eventsRouter.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/events", );
+router.get("/events/:id", );
+router.post("/events", );
+router.put("/events/:id", );
+router.post("/events/:eventId/attendees/:attendeeId");
+router.delete ("/events/:eventId/attendees/:attendeeId", )
+router.delete ("/events/:id", )
+
+export default router
